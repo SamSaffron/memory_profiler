201609e02cd08d878d4dff5f053355db69fd2992
Dave Gynn <davegynn@gmail.com>
2015-07-09 11:41:12 -0700

    remove full_gc helper methods no longer used

--- lib/memory_profiler/helpers.rb
+++ lib/memory_profiler/helpers.rb
@@ -17,27 +17,5 @@ module MemoryProfiler
     def self.reset_gem_guess_cache
       @@gem_guess_cache = Hash.new
     end
-
-    # helper to work around GC.start not freeing everything
-    def self.full_gc
-      # TODO: discuss with @tmm1 and @ko1 if the while loop is needed
-      GC.start while new_count = decreased_count(new_count)
-    end
-
-    def self.decreased_count(old)
-      count = count_objects
-      if !old || count < old
-        count
-      else
-        nil
-      end
-    end
-
-    def self.count_objects
-      i = 0
-      ObjectSpace.each_object do |obj|
-        i += 1
-      end
-    end
   end
 end
