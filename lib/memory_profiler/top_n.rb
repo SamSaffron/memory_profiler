9d47e4b8e5ebb433a72159cded89003a94211d1b
Dave Gynn <davegynn@gmail.com>
2016-09-18 22:42:32 -0700

    Speed up top_n by ~30-50%
    the performance improvement depends on the number of stats as well
    as their distribution but is faster in all cases.
    
    top_n is not a primary bottleneck compared to object_list and
    the code being profiled itself but can still account for 10-20%
    of the total time.

--- lib/memory_profiler/top_n.rb
+++ lib/memory_profiler/top_n.rb
@@ -4,10 +4,9 @@ module MemoryProfiler
     # Returns results for both memory (memsize summed) and objects allocated (count) as a tuple.
     def top_n(max, metric_method)
 
-      stats_by_metric = self.values.map! { |stat| [stat.send(metric_method), stat.memsize] }
-
-      stat_totals = stats_by_metric.group_by { |metric_value, _memsize| metric_value }.
-          map { |key, values| [key, values.reduce(0) { |sum, item| _key, memsize = item ; sum + memsize }, values.size] }
+      stat_totals = self.values.group_by(&metric_method).map do |metric, stats|
+        [metric, stats.reduce(0) { |sum, stat| sum + stat.memsize }, stats.size]
+      end
 
       stats_by_memsize = stat_totals.sort_by! { |metric, memsize, _count| [-memsize, metric] }.take(max).
           map! { |metric, memsize, _count| { data: metric, count: memsize } }
