21b9ad6e16e8cbc284db83fc2b3ff1b88096b37c
Dave Gynn <davegynn@gmail.com>
2016-10-06 00:01:48 -0700

    prevent conflicts with ruby PrettyPrint
    Handles the special case that Ruby PrettyPrint expects `pretty_print`
    to be a customized pretty printing function for a class

--- test/test_results.rb
+++ test/test_results.rb
@@ -1,9 +1,18 @@
 require_relative 'test_helper'
 
 class TestResults < Minitest::Test
-  def test_pretty_print_works
-    io = StringIO.new
-    MemoryProfiler::Results.new.pretty_print io
+  def test_pretty_print_works_with_no_args
+    assert_output(/^Total allocated/, '') { MemoryProfiler::Results.new.pretty_print }
   end
 
+  def test_pretty_print_works_with_io_arg
+    io = StringIO.new
+    assert_silent { MemoryProfiler::Results.new.pretty_print(io) }
+    assert_match(/^Total allocated/, io.string)
+  end
+
+  def test_no_conflict_with_pretty_print
+    require 'pp'
+    assert_output(/#<MemoryProfiler::Results:\w*>/) { pp(MemoryProfiler::Results.new) }
+  end
 end
