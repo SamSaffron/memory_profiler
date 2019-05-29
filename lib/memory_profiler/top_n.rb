3b02f4f34c634b42631665720e494edf971e6ea2
Ashwin Maroli <ashmaroli@gmail.com>
2019-05-29 15:44:49 +0530

    Separate multiple method chains by newlines

--- lib/memory_profiler/top_n.rb
+++ lib/memory_profiler/top_n.rb
@@ -6,17 +6,26 @@ module MemoryProfiler
     # Returns results for both memory (memsize summed) and objects allocated (count) as a tuple.
     def top_n(max, metric_method)
 
-      stat_totals = self.values.group_by(&metric_method).map do |metric, stats|
-        [metric, stats.reduce(0) { |sum, stat| sum + stat.memsize }, stats.size]
-      end
+      stat_totals =
+        self.values
+          .group_by(&metric_method)
+          .map do |metric, stats|
+            [metric, stats.reduce(0) { |sum, stat| sum + stat.memsize }, stats.size]
+          end
 
-      stats_by_memsize = stat_totals.sort_by! { |metric, memsize, _count| [-memsize, metric] }.take(max).
-        map! { |metric, memsize, _count| { data: metric, count: memsize } }
-      stats_by_count = stat_totals.sort_by! { |metric, _memsize, count| [-count, metric] }.take(max).
-        map! { |metric, _memsize, count| { data: metric, count: count } }
+      stats_by_memsize =
+        stat_totals
+          .sort_by! { |metric, memsize, _count| [-memsize, metric] }
+          .take(max)
+          .map! { |metric, memsize, _count| { data: metric, count: memsize } }
+
+      stats_by_count =
+        stat_totals
+          .sort_by! { |metric, _memsize, count| [-count, metric] }
+          .take(max)
+          .map! { |metric, _memsize, count| { data: metric, count: count } }
 
       [stats_by_memsize, stats_by_count]
-
     end
   end
 end
